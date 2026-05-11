a68f6f9e1e281b05d26556f48f314bfeab43b97f
daniexe <daniiexe@outlook.com>
2026-05-11 19:04:30 +0200

    When login succeeded -> main.fxml will get opened

--- src/main/java/com/companycore/auth/controller/LoginController.java
+++ src/main/java/com/companycore/auth/controller/LoginController.java
@@ -3,9 +3,12 @@ package com.companycore.auth.controller;
 import com.companycore.database.connection.DatabaseConnection;
 import com.companycore.database.dao.employee.EmployeeDaoImpl;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -27,6 +30,26 @@ public class LoginController {
         String username = tfUsername.getText();
         String password = pfPassword.getText();
 
-        employeeDao.employeeLogin(username, password);
+        boolean success = employeeDao.employeeLogin(username, password);
+
+        if (success) {
+            Stage btnStage = (Stage) btnLogin.getScene().getWindow();
+            btnStage.close();
+            openMainMenu();
+        }
+    }
+
+    private void openMainMenu() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/companycore/features/main.fxml"));
+            Stage stage = new Stage();
+            Scene scene = new Scene(loader.load());
+
+            stage.setScene(scene);
+            stage.setMaximized(true);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
